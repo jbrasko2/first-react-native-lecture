005c525bb4a382ce1ff769e89fa722b8212f209b
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-21 10:39:05 -0500

    complete first exercise

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,22 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
+  const name = 'Jordan'
   return (
     <View>
-      <Text style={styles.textStyle}>This is the Components Screen</Text>
-      <Text>Hi there!</Text>
+      <Text style={styles.firstStyle}>Getting started with React Native!</Text>
+      <Text style={styles.secondStyle}>My name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 30,
+  firstStyle: {
+    fontSize: 45,
   },
+  secondStyle: {
+    fontSize: 20
+  }
 });
 
 export default ComponentsScreen;
