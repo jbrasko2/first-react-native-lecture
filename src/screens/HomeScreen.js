e6b98e1d79f1de3b7f34fa64ab9dac18bc35777b
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-21 11:54:50 -0500

    add routing

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,15 @@
 import React from 'react'
-import {
-  Text,
-  StyleSheet,
-  View,
-  Button,
-  TouchableOpacity,
-  Touchable,
-} from 'react-native'
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native'
 
-const HomeScreen = () => {
+const HomeScreen = props => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen</Text>
       <Button
         title='Go to Components Demo'
-        onPress={() => console.log('Button Pressed')}
+        onPress={() => props.navigation.navigate('Components')}
       />
-      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+      <TouchableOpacity onPress={() => props.navigation.navigate('List')}>
         <Text>Go to List Demo</Text>
       </TouchableOpacity>
     </View>
