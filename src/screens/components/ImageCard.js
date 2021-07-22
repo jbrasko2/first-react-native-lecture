8fd17c95f1f2b561604efa025303253afe41f2fe
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-22 12:55:02 -0500

    create ImageCard

---
+++ src/screens/components/ImageCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const ImageCard = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default ImageCard
+
+const styles = StyleSheet.create({})
