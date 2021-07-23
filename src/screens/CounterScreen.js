5c732f83cde44a13229fc29a08b138b025c7cb02
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-23 11:57:52 -0500

    add counter functionality

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 
 const CounterScreen = () => {
+  const [count, setCount] = useState(0)
   return (
     <View>
-      <Text>Current Count: </Text>
+      <Button title='Increase' onPress={() => setCount(count + 1)} />
+      <Button title='Descrease' onPress={() => setCount(count +-1)} />
+      <Text>Current Count: {count}</Text>
     </View>
   )
 }
