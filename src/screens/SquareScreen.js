5aaeec22bc8fa6794ceec82ae183c4c116311366
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-23 14:23:01 -0500

    refactor if statements

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -8,26 +8,20 @@ const SquareScreen = () => {
   const [blue, setBlue] = useState(0)
 
   const setColor = (color, change) => {
-    if (color === 'red') {
-      if (red + change > 255 || red + change < 0) {
+    switch (color) {
+      case 'red':
+        red + change > 255 || red + change < 0 ? null : setRed(red + change)
         return
-      } else {
-        setRed(red + change)
-      }
-    }
-    if (color === 'green') {
-      if (green + change > 255 || green + change < 0) {
+      case 'green':
+        green + change > 255 || green + change < 0
+          ? null
+          : setGreen(green + change)
         return
-      } else {
-        setGreen(green + change)
-      }
-    }
-    if (color === 'blue') {
-      if (blue + change > 255 || blue + change < 0) {
+      case 'blue':
+        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
         return
-      } else {
-        setBlue(blue + change)
-      }
+      default:
+        return
     }
   }
 
