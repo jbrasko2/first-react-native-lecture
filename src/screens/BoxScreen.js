a40cc9b7c161b5b3005b6065fb46475ccbd500a8
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-26 10:31:51 -0500

    create BoxScreen

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const BoxScreen = () => {
+  return (
+    <View>
+      <Text>Box Screen</Text>
+    </View>
+  )
+}
+
+export default BoxScreen
+
+const styles = StyleSheet.create({})
