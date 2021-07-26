d867688567972655a179f1aa5904bb83ce0c9723
Jordan Brasko <71613112+jbrasko2@users.noreply.github.com>
2021-07-26 09:33:41 -0500

    add props to text input

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 
 const TextScreen = () => {
   return (
     <View>
-      <Text>Text Screen</Text>
+      <Text>Enter Name:</Text>
+      <TextInput
+        style={styles.textInputStyle}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
     </View>
   )
 }
 
 export default TextScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textInputStyle: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+})
